dfe95c8551904542c63b9af084e8dc0e1f4bd18d
Thales Martim <thales9696@protonmail.com>
2024-05-05 13:01:17 -0300

    normalize css, DarkModeChange and LanguageChange

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
   var userLanguage = navigator.language || navigator.userLanguage;
   var languagePrefix = userLanguage.substring(0, 2);
@@ -8,27 +8,35 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('language-select').value = 'pt';
     document.getElementById('title-en').style.display = 'none';
     document.getElementById('title-pt').style.display = 'block';
-  } else{
+  } else {
     document.getElementById('language-select').value = 'en';
     document.getElementById('title-en').style.display = 'block';
     document.getElementById('title-pt').style.display = 'none';
   }
 
-  if(prefersDarkMode){
+  if (prefersDarkMode) {
     document.getElementById('checkDarkMode').checked = true;
   }
-  else{
+  else {
     document.getElementById('checkDarkMode').checked = false;
   }
 });
 
-document.getElementById('language-select').addEventListener('change', function() {
-  var language = this.value;
-  if (language === 'en') {
-    document.getElementById('title-en').style.display = 'block';
-    document.getElementById('title-pt').style.display = 'none';
-  } else if (language === 'pt') {
+function languageChange() {
+  var language = document.getElementById('language-select').value;
+  if (language === 'pt') {
     document.getElementById('title-en').style.display = 'none';
     document.getElementById('title-pt').style.display = 'block';
+  } else{
+    document.getElementById('title-en').style.display = 'block';
+    document.getElementById('title-pt').style.display = 'none';
   }
-});
+}
+
+function DarkModeChange() {
+  if (document.getElementById("checkDarkMode").checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
